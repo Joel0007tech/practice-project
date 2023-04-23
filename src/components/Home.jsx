b62fb11a684a75f40289e8bd149ce055f151f8ac
Joel <ajiolajoel@gmail.com>
2023-04-23 20:29:19 +0100

    update to the project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,58 +2,87 @@ import { React, useState } from "react";
 import { FaStream, FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Logo from "../assets/Group 3999.png";
-import avatar from "../assets/avatar.png"
+import avatar from "../assets/avatar.png";
+import socialOne from "../assets/Vector (1).png";
+import socialTwo from "../assets/Vector (2).png";
+import socialThree from "../assets/Vector (3).png";
+import socialFour from "../assets/Vector (4).png";
+import socialFive from "../assets/Vector (5).png";
+import objectFirst from "../assets/Vector 3.png";
+import objectSecond from "../assets/Ellipse 62.png";
 
 const Home = () => {
   const [open, setOpen] = useState(false);
   const color = {
     backgroundColor: "#383838",
   };
-const Font ={
-  fontFamily:"Space Grotesk, sans-serif"
-}
+  const Font = {
+    fontFamily: "Space Grotesk, sans-serif",
+  };
   return (
     <div>
-      <div className="pt-6 h-full w-full sm:h-full sm:w-full" style={color}>
-        <div className="flex p-6">
-          <img src={Logo} alt="" className="px-20 fixed" />
-          <h3 onClick={() => setOpen(!open)}>
+      <div
+        className="pt-6 h-full w-full sm:h-full sm:w-full sm:overflow-hidden"
+        style={color}
+      >
+        <div className=" p-6  hidden 2xl:hidden sm:block overflow-hidden
+            sm:border-b-2 sm:border-white sm:rounded-b-md">
+          <div className="sm:pb-3 sm:pt-3 sm:flex">
+            <img
+              src={Logo}
+              alt=""
+              className="px-20 fixed sm:fixed sm:top-8 sm:right-24"
+            />
             <FaStream
               className="cursor-pointer sm:cursor-pointer text-white sm:text-white
-         text-4xl sm:text-2xl fixed sm:fixed right-60 sm:right-10 font-normal
-        "
+              sm:text-2xl fixed sm:fixed sm:right-10 font-normal sm:top-11 "
+              onClick={() => setOpen(!open)}
             />
-          </h3>
+          </div>
         </div>
         {open && (
-          <div>
+          <div className="hidden 2xl:hidden sm:block overflow-hidden
+          sm:border-b-2 sm:border-white sm:rounded-b-md">
             <ul
-              className="flex justify-center flex-col gap-6 text-center cursor-pointer
+              className="flex justify-center flex-col gap-6 sm:gap-2 text-center cursor-pointer
             pt-18"
             >
-              <li className="text-white font-bold text-3xl">
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold">
                 <Link to="/">Home</Link>
               </li>
-              <li className="text-white font-bold text-3xl">
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold">
                 <Link to="/services">Our services</Link>
               </li>
-              <li className="text-white font-bold text-3xl">
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold">
                 <Link to="/works">Our works</Link>
               </li>
-              <li className="text-white font-bold text-3xl">
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold">
                 <Link to="/about">About Us</Link>
               </li>
-              <li className="text-white font-bold text-3xl">
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold">
                 <Link to="/team">Team</Link>
               </li>
-              <li className="text-white font-bold text-3xl">
+              <li className="text-white font-bold text-3xl sm:text-sm sm:font-semibold">
                 <Link to="/contact">Contact Us</Link>
               </li>
             </ul>
           </div>
         )}
+        <div
+          className="border-[2px] border-purple-500 w-[45px] h-[280px] 
+        2xl:block sm:hidden float-right flex flex-col gap-4 pt-8"
+        >
+          <img src={socialOne} alt="" className="w-[30px] h-[30px] " />
+          <img src={socialTwo} alt="" className="w-[30px] h-[30px] " />
+          <img src={socialThree} alt="" className="w-[30px] h-[30px] " />
+          <img src={socialFour} alt="" className="w-[30px] h-[30px] " />
+          <img src={socialFive} alt="" className="w-[30px] h-[30px] " />
+        </div>
         <section>
-          <h2 className="text-white font-semibold text-7xl text-center leading-tight" style={Font}>
+          <h2
+            className="text-white font-semibold text-7xl sm:text-4xl sm:pt-4 text-center leading-tight"
+            style={Font}
+          >
             Bringing Beautiful <br />
             <span className="text-yellow-200">Web 3.0</span> Experiences
             <br />
@@ -64,84 +93,185 @@ const Font ={
               href=""
               className="text-center bg-purple-800 py-3 px-9 sm:py-2 sm:my-4
               hover hover:bg-white hover:text-purple-800 text-white font-semibold sm:text-sm
-             w-[250px] sm:w-[250px] h-[50px] sm:h-[40px] m-auto rounded-lg" style={Font}>
+             w-[250px] sm:w-[250px] h-[50px] sm:h-[40px] m-auto rounded-lg"
+              style={Font}
+            >
               Let us create magic!
             </a>
           </div>
         </section>
         <section className="pt-20">
+          <div className="flex sm:flex-col">
+            <div className="relative left-[300px] top-[150px] sm:left-3 sm:top-0">
+              <h3 className="text-white text-2xl font-semibold sm:text-center" style={Font}>
+                Let's Play!
+              </h3>
+            </div>
+            <div className="border-[3px] rounded-t-xl border-solid 
+             border-white h-[350px] sm:h-[300px] w-[450px] sm:w-[250px]
+             m-auto pt-4">
+              <img src={avatar} alt="ava" className="m-auto" />
+            </div>
+            <div className=" relative right-[300px] top-[150px] sm:top-3 sm:left-[80px]">
+              <h3 className="text-white text-2xl font-semibold" style={Font}>
+                WE DESIGN !
+              </h3>
+              <h3 className="text-white text-2xl font-semibold" style={Font}>
+                WE DEVELOP !
+              </h3>
+              <h3 className="text-white text-2xl font-semibold" style={Font}>
+                WE INTEGRATE !
+              </h3>
+            </div>
+          </div>
+        </section>
+        <section className="pt-20">
+          <div className="flex justify-center gap-8 sm:gap-4">
+            <div className="border border-white h-8 sm:h-6 w-8 sm:w-6 bg-purple-700 relative top-1 sm:top-1.5"></div>
+            <h3 className="text-white text-4xl sm:text-3xl font-bold" style={Font}>
+              Our Story{" "}
+            </h3>
+            <div className="border border-white h-8 sm:h-6 w-8 sm:w-6 bg-yellow-300 relative top-1 sm:top-1.5"></div>
+          </div>
+          <p className="font-normal leading-normal font-sans text-white text-center p-10 text-lg">
+            Tackling Ace is a web development agency that helps creative studio
+            that timely design
+            <br /> work contributes largely to streamlining our development
+            process. Their edge as UX
+            <br /> specialists stems from their ability to design visually
+            appealing interfaces while
+            <br />
+            understanding how every element is intended to function.
+          </p>
+        </section>
+        <section className="pt-20">
+          <h3
+            style={Font}
+            className="text-white text-center text-4xl sm:2xl font-bold m-auto
+             border-b-[14px] sm:border-b-[10px] border-b-yellow-300 w-[250px] pb-3"
+          >
+            What we do
+          </h3>
+          <div className="flex gap-4 justify-center m-auto pt-10 sm:flex-col">
+            <div
+              className="border-[2px] border-gray-500 h-[350px] w-[280px] sm:h-[250px] sm:w-[250px] 
+                   hover:border-purple-800 hover:border-[5px] sm:m-auto"
+            >
+              <h3 className="text-white text-center text-xl" style={Font}>
+                UI/UX Design
+              </h3>
+              <p className="text-white leading-normal pt-10 text-center sm:text-sm sm:pt-2">
+                Unlock the full potential of your
+                <br />
+                product with our expert UI/UX
+                <br />
+                design services. Our team of
+                <br />
+                designers and researchers work
+                <br />
+                closely with you to create a visually
+                <br />
+                stunning and intuitive user
+                <br />
+                interface, while optimizing the
+                <br /> overall user experience.
+              </p>
+            </div>
+            <div
+              className="border-[2px] border-gray-500 h-[350px] sm:h-[250px] sm:w-[250px] w-[280px]
+                   hover:border-purple-800 hover:border-[5px] sm:m-auto"
+            >
+              <h3 className="text-white text-center text-xl" style={Font}>
+                Full Stack <br /> Web Development
+              </h3>
+              <p className="text-white leading-normal pt-10 text-center sm:text-sm sm:pt-2">
+                Bring your online vision to life with
+                <br />
+                our professional web development
+                <br />
+                services. Our team of experienced
+                <br />
+                developers will work with you to
+                <br />
+                design, develop and launch a<br />
+                custom website that not only looks
+                <br />
+                great, but also performs
+                <br />
+                seamlessly.
+              </p>
+            </div>
+            <div
+              className="border-[2px] border-gray-500 h-[350px] w-[280px] sm:h-[250px] sm:w-[250px] 
+                   hover:border-purple-800 hover:border-[5px] sm:m-auto"
+            >
+              <h3 className="text-white text-center text-xl" style={Font}>
+                Smart Contract <br /> Integration
+              </h3>
+              <p className="text-white leading-normal pt-10 text-center sm:text-sm sm:pt-2">
+                Our team of blockchain experts will
+                <br />
+                work with you to design and
+                <br />
+                implement smart contracts that
+                <br />
+                automate and secure your business
+                <br />
+                processes. From supply chain
+                <br />
+                management to financial
+                <br />
+                transactions, smart contracts can
+                <br />
+                provide a tamper-proof and
+                <br />
+                efficient way to conduct business
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="pt-20">
+          <div className="flex justify-center gap-8 sm:gap-4">
+            <div className="border border-white h-8 sm:h-6 w-8 sm:w-6 bg-yellow-300 relative top-1"></div>
+            <h3 className="text-white text-4xl sm:text-2xl font-bold" style={Font}>
+              Our Process{" "}
+            </h3>
+            <div className="border border-white h-8 sm:h-6 w-8 sm:w-6 bg-purple-700 relative top-1"></div>
+          </div>
           <div className="flex">
-            <div className="relative left-[300px] top-[150px]">
-              <h3 className="text-white text-2xl font-semibold" style={Font}>Let's Play!</h3></div>
-            <div className="border-[3px] rounded-t-xl border-solid  border-white h-[350px] w-[450px] m-auto pt-4">
-           <img src={avatar} alt="ava" className="m-auto"/>
+            <div className="flex">
+              <img src={objectSecond} alt="ellipse" className="h-8 w-8" />
+              <img
+                src={objectFirst}
+                alt="line"
+                className="h-1 w-[200px] bg-white relative top-3.5"
+              />
             </div>
-            <div className=" relative right-[300px] top-[150px]">
-              <h3 className="text-white text-2xl font-semibold" style={Font}>WE DESIGN !</h3>
-            <h3 className="text-white text-2xl font-semibold" style={Font}>WE DEVELOP !</h3>
-            <h3 className="text-white text-2xl font-semibold" style={Font}>WE INTEGRATE !</h3></div>
-
-          </div>
-        </section>
-        <section className="pt-20">
-          <div className="flex justify-center gap-8">
-          <div className="border border-white h-8 w-8 bg-purple-700 relative top-1"></div>
-          <h3 className="text-white text-4xl font-bold" style={Font}>Our Story </h3>
-          <div className="border border-white h-8 w-8 bg-yellow-300 relative top-1"></div>
-          </div>
-         <p className="font-normal leading-normal font-sans text-white text-center p-10 text-lg">
-          Tackling Ace is a web development agency that helps creative
-          studio that timely design<br/> work contributes largely to streamlining
-           our development process. Their edge as UX<br/> specialists stems from
-            their ability to design visually appealing interfaces while<br/>
-             understanding how every element is intended to function.</p>
-        </section>
-        <section className="pt-20">
-            <h3 style={Font} className="text-white text-center text-4xl font-bold m-auto
-             border-b-[14px] border-b-yellow-300 w-[250px] pb-3">
-              What we do</h3>
-            <div className="flex gap-4 justify-center m-auto pt-10">
-                  <div
-                   className="border-[2px] border-gray-500 h-[350px] w-[280px]
-                   hover:border-purple-800 hover:border-[5px]">
-                    <h3 className="text-white text-center text-xl" style={Font}>UI/UX Design</h3>
-                    <p className="text-white leading-normal pt-10 text-center">
-                      Unlock the full potential of your<br/> 
-                    product with our expert UI/UX<br/>
-                     design services. Our team of<br/>
-                      designers and researchers work<br/> 
-                     closely with you to create a visually<br/>
-                      stunning and intuitive user<br/>
-                       interface, while optimizing the<br/> overall user experience.
- </p></div>
-                  <div
-                   className="border-[2px] border-gray-500 h-[350px] w-[280px]
-                   hover:border-purple-800 hover:border-[5px]">
-                    <h3 className="text-white text-center text-xl" style={Font}>Full Stack <br/> Web Development</h3>
-                    <p className="text-white leading-normal pt-10 text-center">
-                      Bring your online vision to life with<br/>
-                     our professional web development<br/>
-                     services. Our team of experienced<br/>
-                      developers will work with you to<br/>
-                      design, develop and launch a<br/>
-                      custom website that not only looks<br/>
-                       great, but also performs<br/>
-                        seamlessly.</p></div>
-                  <div
-                   className="border-[2px] border-gray-500 h-[350px] w-[280px]
-                   hover:border-purple-800 hover:border-[5px]">
-                    <h3 className="text-white text-center text-xl" style={Font}>Smart Contract <br/> Integration</h3>
-                    <p className="text-white leading-normal pt-10 text-center">
-                      Our team of blockchain experts will<br/>
-                     work with you to design and<br/>
-                      implement smart contracts that<br/>
-                       automate and secure your business<br/>
-                        processes. From supply chain<br/>
-                         management to financial<br/>
-                          transactions, smart contracts can<br/>
-                           provide a tamper-proof and<br/>
-                            efficient way to conduct business</p></div>
+            <div className="flex">
+              <img src={objectSecond} alt="ellipse" className="h-8 w-8" />
+              <img
+                src={objectFirst}
+                alt="line"
+                className="h-1 w-[200px] bg-white relative top-3.5"
+              />
             </div>
+            <div className="flex">
+              <img src={objectSecond} alt="ellipse" className="h-8 w-8" />
+              <img
+                src={objectFirst}
+                alt="line"
+                className="h-1 w-[200px] bg-white relative top-3.5"
+              />
+            </div>
+            <div className="flex">
+              <img src={objectSecond} alt="ellipse" className="h-8 w-8" />
+              <img
+                src={objectFirst}
+                alt="line"
+                className="h-1 w-[200px] bg-white relative top-3.5"
+              />
+            </div>
+          </div>
         </section>
         <footer
           className="
@@ -157,6 +287,7 @@ const Font ={
         </footer>
       </div>
     </div>
+    
   );
 };
 
