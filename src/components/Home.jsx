4bda61e0a89e0cf94fb121f83b178f6bc4aa48de
Joel <ajiolajoel@gmail.com>
2023-04-27 08:23:48 +0100

    update to the project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -324,21 +324,22 @@ const Home = () => {
           >
             Some of our works
           </h3>
-          <div className="flex flex-col p-20 gap-14">
-          <div className="relative w-64 h-64">
-      <img
-        className="w-full h-full object-cover"
+          <div className="flex flex-col p-20 gap-14 items-center">
+          <div className="relative w-[600px] h-[450px] right-40">
+            <img
+        className="w-[600px] h-[450px] hover:w-[100px] hover:h-[200px]"
         src={firstImage}
         alt="placeholder"
       />
+      
       <div className="absolute top-0 left-0 w-full h-full flex
        items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
         <p className="text-white font-bold text-lg">Hello there</p>
       </div>
     </div>
-    <div className="relative w-64 h-64">
+    <div className="relative w-[600px] h-[450px] left-40">
       <img
-        className="w-full h-full object-cover"
+        className="w-[600px] h-[450px]"
         src={secondImage}
         alt="placeholder"
       />
